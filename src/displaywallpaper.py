0eab7e79ca6045ff7cd830d4ca3d3354ec99bdcf
Talo Halton <talohalton@gmail.com>
2023-09-05 18:12:52 +0100

    Initial commit

---
+++ src/displaywallpaper.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python
+
+import os
+import json
+import subprocess
+from random import randrange, seed
+from os.path import join, expanduser
+
+LIVE_DATA_FILE = expanduser("~/.wallpaperlaunch")
+
+WALLPAPER_DIR = expanduser("~/Media/Wallpapers/")
+SETS_DIR = expanduser("~/Media/Wallpapers/Sets")
+
+VIDEO_FILETYPES = ["mp4"]
+LIVE_FILETYPES = ["live"]
+
+LEFT_DIR = join(WALLPAPER_DIR, "ActiveL")
+RIGHT_DIR = join(WALLPAPER_DIR, "ActiveR")
+MAIN_DIR = join(WALLPAPER_DIR, "Active")
+
+def getHyprctlOutput(command: str) -> str:
+	return json.loads(subprocess.check_output(f"hyprctl {command} -j", shell = True))
+
+def getMonitorName(left: bool) -> str:
+	target_pos = 0 if left else 1920
+	
+	monitors: list[dict] = getHyprctlOutput("monitors")
+	for monitor in monitors:
+		if monitor["x"] == target_pos:
+			return monitor["name"]
+
+	return monitors[0]["name"]
+
+def setImageWallpaper(left: bool, path: str) -> subprocess.Popen:
+	monitor = getMonitorName(left)
+	return subprocess.Popen(["swaybg", "-o", monitor, "-i", path])
+
+def setVideoWallpaper(left: bool, path: str):
+	monitor = getMonitorName(left)
+	return subprocess.Popen(["mpvpaper", monitor, path])
+
+def getVideoDuration(path: str):
+	return subprocess.check_output(f"ffmpeg -i {path} 2>&1 | grep \"Duration\"| cut -d ' ' -f 4 | sed s/,//", shell = True).decode().strip()
+
+def setLiveWallpaper(left: bool, path: str):
+	monitor = getMonitorName(left)
+
+	f = open(path, "r")
+	data = json.loads(f.read())
+	f.close()
+
+	video = join(os.path.dirname(path), data["video"])
+
+	socket = "/tmp/wp-" + monitor
+	options = "input-ipc-server=" + socket
+
+	loop_position = data.get("loop-position")
+	if loop_position is not None:
+		options += " --ab-loop-a=" + data["loop-position"]
+		
+		duration = getVideoDuration(video).removeprefix("00:")
+		options += " --ab-loop-b=" + duration
+
+	writeLiveData(monitor, socket, path)
+
+	args = ["mpvpaper", "-o", options, monitor, video]
+	return subprocess.Popen(args)
+
+def writeLiveData(monitor: str, socket: str, live_path: str):
+	data = {}
+
+	if os.path.isfile(LIVE_DATA_FILE):
+		f = open(LIVE_DATA_FILE, "r")
+		data = json.loads(f.read())
+		f.close()
+
+	data[monitor] = {"socket": socket, "live": live_path}
+
+	f = open(LIVE_DATA_FILE, "w")
+	f.write(json.dumps(data))
+	f.close()
+
+def setWallpapers(left_path: str, right_path: str):
+	if os.path.isfile(LIVE_DATA_FILE):
+		os.remove(LIVE_DATA_FILE)
+	os.system("killall swaybg")
+	os.system("killall mpvpaper")
+
+	global left_proc
+	global right_proc
+	left_proc = None
+	right_proc = None
+
+	def tryFileTypes(types: list[str], onFound):
+		global left_proc
+		global right_proc
+
+		for type in types:
+			if left_path is not None and left_proc is None and left_path.endswith("." + type):
+				left_proc = onFound(True, left_path)
+			if right_path is not None and right_proc is None and right_path.endswith("." + type):
+				right_proc = onFound(False, right_path)
+
+	tryFileTypes(VIDEO_FILETYPES, setVideoWallpaper)
+	tryFileTypes(LIVE_FILETYPES, setLiveWallpaper)
+
+	if left_path is not None and left_proc is None:
+		left_proc = setImageWallpaper(True, left_path)
+	if right_path is not None and right_proc is None:
+		right_proc = setImageWallpaper(False, right_path)
+
+	try:
+		if left_proc is not None:
+			left_proc.wait()
+		if right_proc is not None:
+			right_proc.wait()
+	except:
+		if os.path.isfile(LIVE_DATA_FILE):
+			os.remove(LIVE_DATA_FILE)
+
+		if left_proc is not None:
+			left_proc.kill()
+		if right_proc is not None:
+			right_proc.kill()
+
+def getRandomWallpaper(directory: str, used_files: list):
+	files = []
+
+	for file in os.listdir(directory):
+		if file.startswith("."):
+			continue
+
+		file_path = join(directory, file)
+		if file.endswith(".set"):
+			f = open(file_path, "r")
+			set_content = f.read().strip()
+			f.close()
+			
+			set_path = join(SETS_DIR, set_content)
+
+			if not os.path.isdir(set_path):
+				print("Set not found: " + set_path)
+				continue
+
+			for set_file in os.listdir(set_path):
+				if set_file.startswith("."):
+					continue
+
+				set_file_path = join(set_path, set_file)
+				if set_file_path in used_files:
+					continue
+				files.append(set_file_path)
+
+			continue
+
+		if file_path in used_files:
+			continue
+
+		files.append(file_path)
+
+	if len(files) > 0:
+		ret = files[randrange(len(files))]
+		used_files.append(ret)
+		return ret
+
+	return None
+
+def main():
+	seed()
+
+	wallpapers = {"left": "", "right": ""}
+	used_files = []
+
+	def getSide(side, used):
+		if os.path.isdir(side[1]):
+			file = getRandomWallpaper(side[1], used)
+			if file is not None:
+				wallpapers[side[0]] = file
+				return
+		
+		wallpapers[side[0]] = getRandomWallpaper(MAIN_DIR, used)
+
+	for side in (("left", LEFT_DIR), ("right", RIGHT_DIR)):
+		getSide(side, used_files)
+
+	if wallpapers["right"] is None:
+		getSide(("right", RIGHT_DIR), [])
+
+	print(wallpapers)
+
+	setWallpapers(wallpapers["left"], wallpapers["right"])
+
+if __name__ == "__main__":
+	main()
---
+++ src/switchworkspace.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python
+
+import os
+import json
+import subprocess
+import argparse
+from os.path import expanduser
+
+LIVE_DATA_FILE = expanduser("~/.wallpaperlaunch")
+
+def performWorkspaceChangedAction(data: dict, action: dict, current_workspace: dict, previous_workspace: dict, going_left: bool, is_session_new: bool, workspaces: list[dict]):
+	monitor = current_workspace["monitor"]
+	socket = data[monitor]["socket"]
+
+	only_on_empty = action.get("only-on-empty") == True
+	if only_on_empty and current_workspace["windows"] > 0:
+		return
+
+	only_going_to_right = action.get("only-going-to-right") == True
+	if only_going_to_right and going_left:
+		return
+
+	is_new = current_workspace["windows"] == 0
+	if is_new:
+		for workspace in workspaces:
+			if workspace["monitor"] != monitor:
+				continue
+
+			if workspace["id"] > current_workspace["id"]:
+				is_new = False
+
+	only_on_new = action.get("only-on-new") == True
+	if only_on_new and not is_new:
+		return
+	
+	only_on_session_new = action.get("only-on-session-new") == True
+	if only_on_session_new and (not is_session_new or not is_new):
+		return
+
+	seek_position = action.get("seek-position")
+	if seek_position is not None:
+		os.system(f"echo 'seek {seek_position} absolute' | socat - {socket}")
+
+def onParseFailed(exception: Exception):
+	relative_path = os.path.relpath(
+		LIVE_DATA_FILE,
+		expanduser("~/")
+	)
+	os.system(f"notify-send 'Parsing ~/{relative_path} failed' '{exception}'")
+
+def getData():
+	if not os.path.isfile(LIVE_DATA_FILE):
+		return None
+	
+	f = open(LIVE_DATA_FILE, "r")
+	
+	try:
+		return json.loads(f.read())
+	except Exception as e:
+		onParseFailed(e)
+		raise e
+	finally:
+		f.close()
+
+def setData(data: dict):
+	f = open(LIVE_DATA_FILE, "w")
+	f.write(json.dumps(data))
+	f.close()
+
+def getHyprctlOutput(command: str) -> str:
+	return json.loads(subprocess.check_output(f"hyprctl {command} -j", shell = True))
+
+def getMonitorName(left: bool) -> str:
+	target_pos = 0 if left else 1920
+	
+	monitors: list[dict] = getHyprctlOutput("monitors")
+	for monitor in monitors:
+		if monitor["x"] == target_pos:
+			return monitor["name"]
+
+	return monitors[0]["name"]
+
+def switchWorkspace(left: bool):
+	workspaces = getHyprctlOutput("workspaces")
+	original_workspace = getHyprctlOutput("activeworkspace")
+	monitor = original_workspace["monitor"]
+
+	# Don't allow a workspace to be created if already in an empty one
+	if not left and original_workspace["windows"] == 0:
+		
+		allow_switch = False
+		for workspace in workspaces:
+			if workspace["monitor"] != monitor:
+				continue
+
+			# Allow switch if there is an existing workspace to the right
+			if workspace["id"] > original_workspace["id"]:
+				allow_switch = True
+		
+		if not allow_switch:
+			return
+
+	os.system("hyprctl dispatch workspace " + ("r-1" if left else "r+1"))
+	
+	data = getData()
+	if data is None or not monitor in data:
+		return
+
+	f = open(data[monitor]["live"], "r")
+	
+	try:
+		live_data = json.loads(f.read())
+	except Exception as e:
+		onParseFailed(e)
+		raise e
+	finally:
+		f.close()
+
+	action_config = live_data.get("on-workspace-changed")
+	if action_config is None:
+		return
+
+	opened_workspaces = data[monitor].get("opened_workspaces")
+	if opened_workspaces is None:
+		opened_workspaces = [original_workspace["id"]]
+		data[monitor]["opened_workspaces"] = opened_workspaces
+	
+	new_workspace = getHyprctlOutput("activeworkspace")
+	
+	is_new = False
+	if not new_workspace["id"] in opened_workspaces:
+		is_new = True
+		opened_workspaces.append(new_workspace["id"])
+
+	setData(data)
+
+	performWorkspaceChangedAction(data, action_config, new_workspace, original_workspace, left, is_new, workspaces)
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("direction", choices=["left", "right"])
+	
+	args = parser.parse_args()
+	
+	left = args.direction == "left"
+	switchWorkspace(left)
+
+if __name__ == "__main__":
+	main()
